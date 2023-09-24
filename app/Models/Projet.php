4438020ed5b0d8dd626a52ea5c960d762684bed2
vignemanuelh <emanboyhoudji@gmail.com>
2023-09-24 18:12:37 +0100

    V1

---
+++ app/Models/Projet.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Projet extends Model
+{
+    use HasFactory;
+
+    protected $table = 'projets';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'financial_goal',
+        'state',
+        'creation_date',
+    ];
+}
